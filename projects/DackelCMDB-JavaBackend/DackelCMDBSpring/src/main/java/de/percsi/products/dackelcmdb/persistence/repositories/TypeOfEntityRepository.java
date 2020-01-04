ceba8353b60aa3b90d7092f7b9b9ce3ae34bc79c
Istvan Percsi <istvanpercsi@gmail.com>
2020-01-04 22:30:49 +0100

    Type Of entites remove unused imports

--- projects/DackelCMDB-JavaBackend/DackelCMDBSpring/src/main/java/de/percsi/products/dackelcmdb/persistence/repositories/TypeOfEntityRepository.java
+++ projects/DackelCMDB-JavaBackend/DackelCMDBSpring/src/main/java/de/percsi/products/dackelcmdb/persistence/repositories/TypeOfEntityRepository.java
@@ -3,7 +3,7 @@ package de.percsi.products.dackelcmdb.persistence.repositories;
 import de.percsi.products.dackelcmdb.persistence.model.TypeOfEntityModelDB;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Service;
+
 
 import java.util.Optional;
 
