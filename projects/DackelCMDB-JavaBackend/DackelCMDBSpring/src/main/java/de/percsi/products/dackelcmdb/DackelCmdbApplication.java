4853349e2e7cd87f6876c522fe5625130c771a12
Istvan Percsi <istvanpercsi@gmail.com>
2022-01-01 23:14:29 +0100

    swagger 2 ui generator works

--- projects/DackelCMDB-JavaBackend/DackelCMDBSpring/src/main/java/de/percsi/products/dackelcmdb/DackelCmdbApplication.java
+++ projects/DackelCMDB-JavaBackend/DackelCMDBSpring/src/main/java/de/percsi/products/dackelcmdb/DackelCmdbApplication.java
@@ -1,4 +1,4 @@
-package de.percsi.products.dackelcmdb.dackelcmdb;
+package de.percsi.products.dackelcmdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- projects/DackelCMDB-JavaBackend/DackelCMDBSpring/src/test/java/de/percsi/products/dackelcmdb/DackelCmdbApplicationTests.java
+++ projects/DackelCMDB-JavaBackend/DackelCMDBSpring/src/test/java/de/percsi/products/dackelcmdb/DackelCmdbApplicationTests.java
@@ -1,4 +1,4 @@
-package de.percsi.products.dackelcmdb.dackelcmdb;
+package de.percsi.products.dackelcmdb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
