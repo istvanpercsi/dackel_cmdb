9403fc01fcc5ddb9b8909dd2cd68573c0227add7
Istvan Percsi <istvanpercsi@gmail.com>
2022-01-11 21:45:26 +0100

    Rework API Vavr list can be documented by srpingdoc, with type parameter as well.

--- projects/DackelCMDB-JavaBackend/DackelCMDBSpring/controller/src/main/java/de/percsi/product/entitymanager/configuration/VavrSeqSupportConverterForSpringDoc.java
+++ projects/DackelCMDB-JavaBackend/DackelCMDBSpring/controller/src/main/java/de/percsi/product/entitymanager/configuration/VavrSeqSupportConverterForSpringDoc.java
@@ -1,25 +1,35 @@
 package de.percsi.product.entitymanager.configuration;
 
 import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.SimpleType;
 import io.swagger.v3.core.converter.AnnotatedType;
 import io.swagger.v3.core.converter.ModelConverter;
 import io.swagger.v3.core.converter.ModelConverterContext;
 import io.swagger.v3.core.util.Json;
 import io.swagger.v3.oas.models.media.ArraySchema;
 import io.swagger.v3.oas.models.media.Schema;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Type;
 import java.util.Iterator;
 import java.util.List;
 
 @Component
 public class VavrSeqSupportConverterForSpringDoc implements ModelConverter {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Override
     public Schema resolve(AnnotatedType annotatedType, ModelConverterContext context, Iterator<ModelConverter> chain) {
         JavaType javaType = Json.mapper().constructType(annotatedType.getType());
         if (javaType != null) {
             Class<?> cls = javaType.getRawClass();
             if (io.vavr.collection.Seq.class.isAssignableFrom(cls)) {
+                JavaType typeParamType = objectMapper.getTypeFactory().constructType(javaType.getBindings().getBoundType(0));
+                JavaType listJavaType = objectMapper.getTypeFactory().constructCollectionType(List.class,typeParamType);
                 annotatedType =
                         new AnnotatedType()
                                 .type(javaType.containedType(0))
@@ -30,8 +40,9 @@ public class VavrSeqSupportConverterForSpringDoc implements ModelConverter {
                                 .resolveAsRef(annotatedType.isResolveAsRef())
                                 .jsonViewAnnotation(annotatedType.getJsonViewAnnotation())
                                 .propertyName(annotatedType.getPropertyName())
-                                .skipOverride(true);
-                return new ArraySchema().items(this.resolve(annotatedType, context, chain));
+                                .skipOverride(true)
+                                .type(listJavaType);
+                return this.resolve(annotatedType,context,chain);
             }
         }
         return (chain.hasNext()) ? chain.next().resolve(annotatedType, context, chain) : null;
