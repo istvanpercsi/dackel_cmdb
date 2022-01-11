31b8431257f56f0274e94b689ce7d8f152e23db8
Istvan Percsi <istvanpercsi@gmail.com>
2022-01-11 22:35:59 +0100

    Rework API
    Types for controllers.

---
+++ projects/DackelCMDB-JavaBackend/DackelCMDBSpring/controller/src/main/java/de/percsi/product/entitymanager/controller/domain/ExtendedPropertyResponse.java
@@ -0,0 +1,59 @@
+package de.percsi.product.entitymanager.controller.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Value;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.Valid;
+import java.time.OffsetDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * ExtendedPropertyResponse
+ */
+@Value
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@Builder
+public class ExtendedPropertyResponse   {
+  @JsonProperty("id")
+  @Builder.Default
+  UUID id = null;
+
+  @JsonProperty("name")
+  @Builder.Default
+  String name = null;
+
+  @JsonProperty("type")
+  @Builder.Default
+  TypeOfProperty type = null;
+
+  @JsonProperty("multiplicity")
+  @Builder.Default
+  MultiplicityOfProperty multiplicity = null;
+
+  @JsonProperty("format")
+  @Builder.Default
+  String format = null;
+
+  @JsonProperty("createDate")
+  @Builder.Default
+  OffsetDateTime createDate = null;
+
+  @JsonProperty("modifyDate")
+  @Builder.Default
+  OffsetDateTime modifyDate = null;
+
+  @JsonProperty("creator")
+  @Builder.Default
+  String creator = null;
+
+  @JsonProperty("modifier")
+  @Builder.Default
+  String modifier = null;
+
+}
