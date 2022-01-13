3fe146c52ecc0e7c4dab7e77ab79ea79b6dd2412
Istvan Percsi <istvanpercsi@gmail.com>
2022-01-13 21:43:00 +0100

    Rework API
    PropertiesApi is created. But not jet implemented.

---
+++ projects/DackelCMDB-JavaBackend/DackelCMDBSpring/controller/src/main/java/de/percsi/product/entitymanager/controller/PropertiesApiImpl.java
@@ -0,0 +1,10 @@
+package de.percsi.product.entitymanager.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+public class PropertiesApiImpl implements PropertiesApi {
+
+}
